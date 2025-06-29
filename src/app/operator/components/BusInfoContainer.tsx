adf7d10e781cd90915f02e50adf335553069626f
spookyexe <gianepanto@gmail.com>
2025-06-29 17:22:03 +0800

    REFACTOR: Enhance operator dashboard UI and structure

--- src/app/operator/components/BusInfoContainer.tsx
+++ src/app/operator/components/BusInfoContainer.tsx
@@ -1,29 +1,23 @@
 import Button from '../../../components/Button';
 import type { BusInformationType, TimelineInformationType } from '@/type';
 import { CircleIcon } from '@/components/Icons';
+import Container from '@/components/Container';
+
+import {
+  CardContainer,
+  CardHeader,
+  CardBody,
+  CardFooter,
+} from '@/components/Card';
 
 function StatsCard(props: { label: string; value: number | string }) {
   return (
-    <div className="flex w-[250px] flex-col items-center justify-center bg-[#f5f5f5] p-3">
+    <Container className="flex w-full flex-col items-center justify-center">
       <h1 className="text-primary text-2xl font-bold sm:text-3xl lg:text-4xl">
         {props.value}
       </h1>
-      <p>{props.label}</p>
-    </div>
-  );
-}
-
-function DetailsCard(props: {
-  label: string;
-  value: number | string | null | undefined;
-}) {
-  return (
-    <div className="border-outline flex items-center justify-between border-1 p-2">
-      <p className="text-[12px] sm:text-[14px] lg:text-[16px]">{props.label}</p>
-      <p className="text-[12px] font-bold sm:text-[14px] lg:text-[16px]">
-        {props.value}
-      </p>
-    </div>
+      <p className="text-center text-xs md:text-sm">{props.label}</p>
+    </Container>
   );
 }
 
@@ -42,14 +36,12 @@ function TimelineCard(props: { TimelineInfo: TimelineInformationType }) {
     <div className="flex items-center justify-start gap-10 p-2">
       <div className="flex w-1/3 items-center justify-start gap-4">
         <CircleIcon className="bg-primary h-[15px] w-[15px] rounded-full" />
-        <p className="text-[12px] sm:text-[14px] lg:text-[16px]">
+        <p className="text-sm font-medium md:text-lg">
           {props.TimelineInfo['timestamp']}
         </p>
       </div>
       <div className="flex w-1/2 flex-col items-start justify-start">
-        <p className="text-[12px] font-bold sm:text-[14px] lg:text-[16px]">
-          {statusDescriptions[statusCode]}
-        </p>
+        <p className="text-sm md:text-lg">{statusDescriptions[statusCode]}</p>
       </div>
     </div>
   );
@@ -63,76 +55,66 @@ function BusCard(props: {
   let StatusClass = 'text-black';
   switch (status) {
     case 'active':
-      StatusClass = 'text-green-600 font-bold';
+      StatusClass = 'text-green-600 font-bold bg-green-100 border-green-300';
       break;
     case 'maintenance':
-      StatusClass = 'text-red-600 font-bold';
+      StatusClass = 'text-red-600 font-bold bg-red-100 border-red-300';
       break;
     case 'in transit':
-      StatusClass = 'text-yellow-500 font-bold';
+      StatusClass = 'text-yellow-500 font-bold bg-yellow-100 border-yellow-300';
       break;
     default:
       StatusClass = 'text-black';
   }
 
   return (
-    <div className="flex h-full w-full flex-col sm:p-3">
-      <div className="border-outline bg-neutral flex items-center justify-between border-1 p-3 sm:p-4 lg:p-5">
+    <CardContainer className="border-outline flex h-full w-full flex-col border-1">
+      <CardHeader className="!bg-neutral flex items-center justify-between !text-black">
         <h1 className="text-[16px] font-extrabold sm:text-[18px] lg:text-2xl">
           Bus #{props.BusInfo['bus_id']}
         </h1>
-        <p className={StatusClass}>{props.BusInfo['bus_status']}</p>
-      </div>
-      <div className="grid h-full grid-cols-2 grid-rows-2 gap-5 p-3 sm:p-5">
-        <div className="">
-          <p className="text-[12px] sm:text-[14px] lg:text-[16px]">Route</p>
-          <p className="text-[12px] font-bold sm:text-[14px] lg:text-[16px]">
-            {props.BusInfo['route_id']}
-          </p>
+        <span
+          className={`inline-block rounded-full px-3 py-1 text-xs font-semibold ${StatusClass} border-1`}
+        >
+          {props.BusInfo['bus_status']}
+        </span>
+      </CardHeader>
+      <CardBody className="grid h-full grid-cols-2 grid-rows-2 gap-5 p-3 sm:p-5">
+        <div>
+          <p className="text-sm font-semibold md:text-lg">Route:</p>
+          <p className="text-xs md:text-sm">{props.BusInfo['route_id']}</p>
         </div>
-        <div className="">
-          <p className="text-[12px] sm:text-[14px] lg:text-[16px]">
-            Current Driver Name
-          </p>
-          <p className="text-[12px] font-bold sm:text-[14px] lg:text-[16px]">
-            {props.BusInfo['driver_name']}
-          </p>
+        <div>
+          <p className="text-sm font-semibold md:text-lg">Bus Driver:</p>
+          <p className="text-xs md:text-sm">{props.BusInfo['driver_name']}</p>
         </div>
-        <div className="">
-          <p className="text-[12px] sm:text-[14px] lg:text-[16px]">
-            Current Location
-          </p>
-          <p className="text-[12px] font-bold sm:text-[14px] lg:text-[16px]">
-            {props.BusInfo['curr_location']}
-          </p>
+        <div>
+          <p className="text-sm font-semibold md:text-lg">Current Location:</p>
+          <p className="text-xs md:text-sm">{props.BusInfo['curr_location']}</p>
         </div>
-        <div className="">
-          <p className="text-[12px] sm:text-[14px] lg:text-[16px]">
-            Passenger Count
-          </p>
-          <p className="text-[12px] font-bold sm:text-[14px] lg:text-[16px]">
+        <div>
+          <p className="text-sm font-semibold md:text-lg">Passenger Count:</p>
+          <p className="text-xs md:text-sm">
             {props.BusInfo['passenger_count']}
           </p>
         </div>
-      </div>
-      <div className="w-1xl border-outline bg-neutral grid grid-cols-2 grid-rows-1 justify-items-center gap-4 border-1 p-3 sm:p-5">
+      </CardBody>
+      <CardFooter>
         <Button
           onClick={() => props.OnClick(props.BusInfo)}
           className="w-full"
           variant="solid"
         >
-          <p className="text-[12px] font-bold sm:text-[14px] lg:text-[16px]">
-            View History
-          </p>
+          <p className="text-xs md:text-sm">View Details</p>
         </Button>
-        <Button className="w-full" variant="outline">
+        {/* <Button className="w-full" variant="outline">
           <p className="text-[12px] font-bold text-black sm:text-[14px] lg:text-[16px]">
             Open Map
           </p>
-        </Button>
-      </div>
-    </div>
+        </Button> */}
+      </CardFooter>
+    </CardContainer>
   );
 }
 
-export { BusCard, StatsCard, TimelineCard, DetailsCard };
+export { BusCard, StatsCard, TimelineCard };
--- src/app/operator/page.tsx
+++ src/app/operator/page.tsx
@@ -1,10 +1,11 @@
+import { toast } from 'sonner';
 import {
   CardContainer,
   CardHeader,
   CardBody,
   CardFooter,
 } from '@/components/Card';
-import { CloseIcon, AlertIcon, RefreshIcon } from '@/components/Icons';
+import { CloseIcon, RefreshIcon, BusIcon } from '@/components/Icons';
 import type {
   BusInformationType,
   StatsInformationType,
@@ -14,86 +15,83 @@ import {
   BusCard,
   StatsCard,
   TimelineCard,
-  DetailsCard,
 } from './components/BusInfoContainer';
 import { useState } from 'react';
 import Button from '@/components/Button';
 
 import Dialog from '@/components/Dialog';
 
-const StatsData: StatsInformationType[] = [
-  {
-    bus_active: 3,
-    bus_maintenance: 12,
-    on_time_performance: 89,
-    total_passenger_count: 1234,
-  },
-];
+const StatsData: StatsInformationType = {
+  bus_active: 3,
+  bus_maintenance: 12,
+  on_time_performance: 89,
+  total_passenger_count: 1234,
+};
 
 const TimelineData: { bus_id: number; timeline: TimelineInformationType[] }[] =
   [
     {
       bus_id: 1,
       timeline: [
-        { timestamp: '8:00 A.M', status_code: 1 },
-        { timestamp: '9:30 A.M', status_code: 2 },
-        { timestamp: '11:30 A.M', status_code: 3 },
+        { timestamp: '8:00 AM', status_code: 1 },
+        { timestamp: '9:30 AM', status_code: 2 },
+        { timestamp: '11:30 AM', status_code: 3 },
       ],
     },
     {
       bus_id: 2,
       timeline: [
-        { timestamp: '12:00 P.M', status_code: 1 },
-        { timestamp: '12:30 SP.M', status_code: 5 },
-        { timestamp: '1:30 P.M', status_code: 2 },
+        { timestamp: '12:00 PM', status_code: 1 },
+        { timestamp: '12:30 PM', status_code: 5 },
+        { timestamp: '1:30 PM', status_code: 2 },
       ],
     },
     {
       bus_id: 3,
       timeline: [
-        { timestamp: '12:00 P.M', status_code: 1 },
-        { timestamp: '12:30 P.M', status_code: 2 },
-        { timestamp: '1:30 P.M', status_code: 5 },
+        { timestamp: '12:00 PM', status_code: 1 },
+        { timestamp: '12:30 PM', status_code: 2 },
+        { timestamp: '1:30 PM', status_code: 5 },
       ],
     },
     {
       bus_id: 4,
       timeline: [
-        { timestamp: '12:00 P.M', status_code: 1 },
-        { timestamp: '12:30 P.M', status_code: 3 },
-        { timestamp: '1:30 P.M', status_code: 4 },
+        { timestamp: '12:00 PM', status_code: 1 },
+        { timestamp: '12:30 PM', status_code: 3 },
+        { timestamp: '1:30 PM', status_code: 4 },
       ],
     },
     {
       bus_id: 5,
       timeline: [
-        { timestamp: '12:00 P.M', status_code: 1 },
-        { timestamp: '12:30 P.M', status_code: 2 },
-        { timestamp: '1:30 P.M', status_code: 4 },
+        { timestamp: '12:00 PM', status_code: 1 },
+        { timestamp: '12:30 PM', status_code: 2 },
+        { timestamp: '1:30 PM', status_code: 4 },
       ],
     },
     {
       bus_id: 6,
       timeline: [
-        { timestamp: '12:00 P.M', status_code: 1 },
-        { timestamp: '12:30 P.M', status_code: 2 },
-        { timestamp: '1:30 P.M', status_code: 2 },
+        { timestamp: '12:00 PM', status_code: 1 },
+        { timestamp: '12:30 PM', status_code: 2 },
+        { timestamp: '1:30 PM', status_code: 2 },
       ],
     },
     {
       bus_id: 7,
       timeline: [
-        { timestamp: '12:00 P.M', status_code: 1 },
-        { timestamp: '12:30 P.M', status_code: 4 },
-        { timestamp: '1:30 P.M', status_code: 2 },
+        { timestamp: '12:00 PM', status_code: 1 },
+        { timestamp: '12:30 PM', status_code: 4 },
+        { timestamp: '1:30 PM', status_code: 2 },
       ],
     },
     {
       bus_id: 8,
       timeline: [
-        { timestamp: '12:00 P.M', status_code: 1 },
-        { timestamp: '12:30 P.M', status_code: 3 },
-        { timestamp: '1:30 P.M', status_code: 4 },
+        { timestamp: '12:00 PM', status_code: 1 },
+        { timestamp: '12:30 PM', status_code: 3 },
+        { timestamp: '1:30 PM', status_code: 4 },
       ],
     },
   ];
@@ -178,124 +176,177 @@ const Operator = () => {
   const [selectedBusId, setSelectedBusId] = useState<number | null>(null);
   const selectedBus = BusData.find((bus) => bus.bus_id === selectedBusId);
 
+  const refreshData = () => {
+    toast.success('Data refreshed successfully!');
+  };
+
   return (
-    <CardContainer className="border-primary-light z-1000 border-10">
+    <>
       <Dialog
         open={isModalOpen}
         as="div"
         onClose={() => setIsModalOpen(false)}
-        className="sm:6/10 w-9/10"
+        className="w-[90%] sm:w-4/5 md:w-3/5"
       >
-        <CardContainer className="h-full w-full">
+        <CardContainer>
           <CardHeader className="flex items-center justify-between">
             <h1 className="text-lg font-semibold text-white">Bus Details</h1>
             <Button
               type="button"
               variant="outline"
               onClick={() => setIsModalOpen(false)}
-              className="px-4"
             >
               <CloseIcon className="text-white" />
             </Button>
           </CardHeader>
-          <CardBody>
-            <div className="flex flex-col items-start justify-around gap-4 sm:flex-col lg:flex-row">
-              <div className="w-full p-3">
-                <div className="bg-neutral border-outline border-1 p-3">
-                  <h1 className="text-primary text-[18px] font-bold sm:text-[20px] lg:text-2xl">
-                    Bus Information
-                  </h1>
-                </div>
-                <div className="border-outline border-1">
-                  <DetailsCard label="Bus ID" value={'#' + selectedBusId} />
-                  <DetailsCard label="Status" value={selectedBus?.bus_status} />
-                  <DetailsCard label="Route" value={selectedBus?.route_id} />
-                  <DetailsCard
-                    label="Current Driver"
-                    value={selectedBus?.driver_name}
-                  />
-                  <DetailsCard
-                    label="Current Conductor"
-                    value={selectedBus?.conductor_name}
-                  />
-                  <DetailsCard
-                    label="Passenger Count"
-                    value={selectedBus?.passenger_count + '/60'}
-                  />
-                </div>
+          <CardBody className="flex flex-col items-start justify-evenly gap-4 !p-4 md:!p-6 xl:flex-row">
+            {selectedBus ? (
+              <table className="border-outline w-full rounded-md border-2">
+                <thead>
+                  <tr>
+                    <th
+                      colSpan={2}
+                      className="bg-neutral text-primary border-outline border-2 p-2 text-left sm:text-lg md:p-4 md:text-xl"
+                    >
+                      Bus Information
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white">
+                  <tr className="border-outline border-b-2">
+                    <td className="sm: p-2 text-sm font-semibold md:text-lg">
+                      Bus ID
+                    </td>
+                    <td className="sm: p-2 text-end text-sm md:text-lg">
+                      {selectedBus.bus_id}
+                    </td>
+                  </tr>
+                  <tr className="border-outline border-b-2">
+                    <td className="sm: p-2 text-sm font-semibold md:text-lg">
+                      Route
+                    </td>
+                    <td className="sm: p-2 text-end text-sm md:text-lg">
+                      {selectedBus.route_id}
+                    </td>
+                  </tr>
+                  <tr className="border-outline border-b-2">
+                    <td className="sm: p-2 text-sm font-semibold md:text-lg">
+                      Driver
+                    </td>
+                    <td className="sm: p-2 text-end text-sm md:text-lg">
+                      {selectedBus.driver_name}
+                    </td>
+                  </tr>
+                  <tr className="border-outline border-b-2">
+                    <td className="sm: p-2 text-sm font-semibold md:text-lg">
+                      Conductor
+                    </td>
+                    <td className="sm: p-2 text-end text-sm md:text-lg">
+                      {selectedBus.conductor_name}
+                    </td>
+                  </tr>
+                  <tr className="border-outline border-b-2">
+                    <td className="sm: p-2 text-sm font-semibold md:text-lg">
+                      Passenger Count
+                    </td>
+                    <td className="sm: p-2 text-end text-sm md:text-lg">
+                      {selectedBus.passenger_count}
+                    </td>
+                  </tr>
+                  <tr className="border-outline border-b-2">
+                    <td className="sm: p-2 text-sm font-semibold md:text-lg">
+                      Current Location
+                    </td>
+                    <td className="sm: p-2 text-end text-sm md:text-lg">
+                      {selectedBus.curr_location}
+                    </td>
+                  </tr>
+                  <tr className="border-outline">
+                    <td className="sm: p-2 text-sm font-semibold md:text-lg">
+                      Status
+                    </td>
+                    <td className="sm: p-2 text-end text-sm capitalize md:text-lg">
+                      {selectedBus.bus_status}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            ) : (
+              <div className="sm: text-center text-sm text-gray-500 md:text-lg">
+                No bus selected.
               </div>
+            )}
 
-              <div className="w-full p-3">
-                <div className="bg-neutral border-outline border-1 p-3">
-                  <h1 className="text-primary text-[18px] font-bold sm:text-[20px] lg:text-2xl">
-                    Timeline
-                  </h1>
-                </div>
-                <div className="border-outline border-1">
-                  {TimelineData.filter((t) => t.bus_id === selectedBusId).map(
-                    (timelineObj) =>
-                      timelineObj.timeline.map((data, index) => (
-                        <TimelineCard key={index} TimelineInfo={data} />
-                      ))
-                  )}
-                </div>
+            <div className="w-full">
+              <h1 className="bg-neutral text-primary border-outline border-2 p-2 text-left font-bold sm:text-lg md:p-4 md:text-xl">
+                Timeline
+              </h1>
+              <div className="border-outline border-1">
+                {TimelineData.filter((t) => t.bus_id === selectedBusId).map(
+                  (timelineObj) =>
+                    timelineObj.timeline.map((data, index) => (
+                      <TimelineCard key={index} TimelineInfo={data} />
+                    ))
+                )}
               </div>
             </div>
           </CardBody>
         </CardContainer>
       </Dialog>
-
-      <CardHeader className="flex items-start justify-between">
-        <div>
-          <h1 className="text-start text-2xl font-bold">Operator Dashboard</h1>
-          <p className="text-primary-light text-start text-sm">
-            Metro Transit Corporation
-          </p>
-        </div>
-        <div className="flex gap-4">
-          <Button className="flex gap-2" variant="glass">
+      <CardContainer className="m-auto max-w-[1400px] p-2">
+        <CardHeader className="flex items-start justify-between">
+          <div>
+            <h1 className="flex items-center gap-2 text-2xl font-bold">
+              <BusIcon /> Ceres Liners
+            </h1>
+            <p className="text-primary-light text-start text-sm">
+              Bus Operator Dashboard
+            </p>
+          </div>
+          <Button
+            className="flex items-center justify-between gap-2"
+            variant="glass"
+            onClick={refreshData}
+          >
             <RefreshIcon className="h-[18px] w-[18px]" />
-            <p className="hidden font-semibold sm:block">Refresh Data</p>
+            <p className="hidden font-semibold sm:block">Refresh</p>
           </Button>
-          <Button className="flex gap-2" variant="glass">
-            <AlertIcon className="h-[18px] w-[18px]" />
-            <p className="hidden font-semibold sm:block">Alerts</p>
-          </Button>
-        </div>
-      </CardHeader>
-      <CardBody className="flex w-full flex-col gap-4">
-        <div className="border-neutral grid h-1/2 w-full grid-cols-2 grid-rows-2 justify-items-center border-1 sm:mt-3 sm:mb-3 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-4 lg:grid-rows-1">
-          <StatsCard value={StatsData[0].bus_active} label="Active Buses" />
-          <StatsCard
-            value={StatsData[0].bus_maintenance}
-            label="In Maintenance"
-          />
-          <StatsCard
-            value={StatsData[0].on_time_performance + '%'}
-            label="On-time Performance"
-          />
-          <StatsCard
-            value={StatsData[0].total_passenger_count}
-            label="Total Passenger Count"
-          />
-        </div>
-        <div className="grid grid-cols-1 items-start justify-items-center gap-5 sm:mt-1 sm:mb-1 sm:grid-cols-2 sm:gap-0 lg:mt-3 lg:mb-3 lg:grid-cols-3">
-          {BusData.map((data, index) => (
-            <BusCard
-              key={index}
-              BusInfo={data}
-              OnClick={() => {
-                setSelectedBusId(data.bus_id);
-                setIsModalOpen(true);
-              }}
+        </CardHeader>
+        <CardBody className="flex w-full flex-col gap-4 !px-2 sm:!px-4 lg:!px-6">
+          <div className="mx-2 grid w-full grid-cols-2 items-center justify-items-center gap-4 md:grid-cols-4">
+            <StatsCard value={StatsData.bus_active} label="Active Buses" />
+            <StatsCard
+              value={StatsData.bus_maintenance}
+              label="In Maintenance"
             />
-          ))}
-        </div>
-      </CardBody>
-      <CardFooter className="!justify-center">
-        CIS2104: Information Management II
-      </CardFooter>
-    </CardContainer>
+            <StatsCard
+              value={StatsData.on_time_performance + '%'}
+              label="On-time Performance"
+            />
+            <StatsCard
+              value={StatsData.total_passenger_count}
+              label="Total Passenger Count"
+            />
+          </div>
+
+          <div className="grid grid-cols-1 items-start justify-items-center gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {BusData.map((data, index) => (
+              <BusCard
+                key={index}
+                BusInfo={data}
+                OnClick={() => {
+                  setSelectedBusId(data.bus_id);
+                  setIsModalOpen(true);
+                }}
+              />
+            ))}
+          </div>
+        </CardBody>
+        <CardFooter className="!justify-center">
+          <p className="text-sm">TRIP: Transit Routing & Integrated Payments</p>
+        </CardFooter>
+      </CardContainer>
+    </>
   );
 };
 
