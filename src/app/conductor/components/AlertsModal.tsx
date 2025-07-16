7ad890fc352e064524e820617a672b11ed5dbc88
spookyexe <gianepanto@gmail.com>
2025-07-17 04:22:52 +0800

    UI(conductor): Alert modal

--- src/app/conductor/components/AlertsModal.tsx
+++ src/app/conductor/components/AlertsModal.tsx
@@ -4,6 +4,55 @@ import { AlertIcon, CloseIcon } from '@/components/Icons';
 import Dialog from '@/components/Dialog';
 import { CardContainer, CardBody, CardHeader } from '@/components/Card';
 
+const alertsData = [
+  {
+    id: 1,
+    message: 'Gian at Seat 1A has called for assistance.',
+    read: false,
+  },
+  {
+    id: 2,
+    message: 'Gian at Seat 1B has requested to stop the bus.',
+    read: false,
+  },
+  {
+    id: 3,
+    message: 'Gian at Seat 1C has called for assistance.',
+    read: true,
+  },
+];
+
+const AlertItem = ({
+  alert,
+  onMarkAsRead,
+}: {
+  alert: { id: number; message: string; read: boolean };
+  onMarkAsRead?: (id: number) => void;
+}) => {
+  return (
+    <li
+      className={`flex w-full items-center justify-between rounded-md !border-1 !p-2 ${
+        alert.read
+          ? 'bg-secondary-light border-secondary text-yellow-600'
+          : 'bg-primary-light border-primary text-primary'
+      }`}
+      {...(onMarkAsRead && alert.read
+        ? {}
+        : { onClick: () => onMarkAsRead?.(alert.id) })}
+    >
+      <p className="text-sm">{alert.message}</p>
+      {!alert.read && (
+        <Button
+          variant="outline"
+          className="ml-2 !px-2 !py-1 text-[10px] font-normal"
+        >
+          Read
+        </Button>
+      )}
+    </li>
+  );
+};
+
 const AlertsModal = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
@@ -14,7 +63,9 @@ const AlertsModal = () => {
         onClick={() => setIsOpen(true)}
       >
         <AlertIcon className="!h-4 !w-4" />{' '}
-        <p className="text-xs md:text-sm">Alerts (2)</p>
+        <p className="text-xs md:text-sm">
+          Alerts ({alertsData.filter((alert) => !alert.read).length})
+        </p>
       </Button>
       <Dialog
         open={isOpen}
@@ -34,13 +85,32 @@ const AlertsModal = () => {
             </Button>
           </CardHeader>
           <CardBody>
-            <ul>
-              <li>Aryan</li>
-              <li>Bryan</li>
-              <li>Cryan</li>
-              <li>Dryan</li>
-              <li>Eryan</li>
+            <h2 className="mb-2 text-sm font-medium">Unread Alerts:</h2>
+            <ul className="flex flex-col gap-2">
+              {alertsData.filter((alert) => !alert.read).length > 0 && (
+                <>
+                  {alertsData
+                    .filter((alert) => !alert.read)
+                    .map((alert, index) => (
+                      <AlertItem
+                        key={index}
+                        alert={alert}
+                        onMarkAsRead={(id) => {
+                          console.log(`Marking alert ${id} as read`);
+                        }}
+                      />
+                    ))}
+                  <hr className="border-outline my-4" />
+                </>
+              )}
             </ul>
+
+            {/* Read Alerts */}
+            {alertsData
+              .filter((alert) => alert.read)
+              .map((alert, index) => (
+                <AlertItem key={index} alert={alert} />
+              ))}
           </CardBody>
         </CardContainer>
       </Dialog>
